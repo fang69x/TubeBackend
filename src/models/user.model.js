1124322f190a238c56ab61a575b3ccea75aa5551
fang69x <109065342+fang69x@users.noreply.github.com>
2025-02-22 20:04:37 +0530

    use of bcrypt and jwt

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
- 
+import jwt from 'jsonwebtoken'
+import bcrypt from "bcrypt"
 const userSchema= new Schema({
 
     username:{
@@ -48,5 +49,40 @@ const userSchema= new Schema({
     timestamps:true,
 }
 );
+
+userSchema.pre("save",async function (next){
+    if(!this.isModeified("password")) return next();
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
+ userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+ }
+ userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresInL:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+ }
+
+ userSchema.methods.generateAccessToken=function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresInL:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+ }
 
 export const User= mongoose.model("User", userSchema)
