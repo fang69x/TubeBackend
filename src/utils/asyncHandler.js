1e8e895e332126277ae27f17379698d90bc47570
fang69x <109065342+fang69x@users.noreply.github.com>
2025-04-02 00:18:01 +0530

    async handler updated

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -12,9 +12,11 @@
 
 
  //option 2
- const asyncHandler=(requestHandler)=>{
-    return (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
+ const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
- }
- export {asyncHandler}
+}
+
+
+export { asyncHandler }
